d8d9d406f2d79db8f84aa42d459e170fee18ec24
BYUMVUHOREAimable <122086815+BYUMVUHOREAimable@users.noreply.github.com>
2023-05-21 14:52:42 +0200

    Addition of Byumvuhore component

---
+++ src/Components/Byumvuhore.js
@@ -0,0 +1,9 @@
+const Byumvuhore = () => {
+    let names = "BAI Programmer"
+    let myDiv = <div>God Be with us all over we are</div>
+    return <div>
+        <h1>Hello {names}, Good morning</h1>
+        {myDiv}
+    </div>
+}
+export default Byumvuhore
